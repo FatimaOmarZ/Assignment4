4d81db8700043b06169489fe29884516b0fbd1ee
FatimaOmarZ <omar5080@mylaurier.ca>
2021-07-26 12:49:31 -0400

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -15,16 +15,32 @@
 #define NUMBER_OF_CUSTOMERS 5
 #define NUMBER_OF_RESOURCES 4
 
-typedef struct banker{
-    /* the available amount of each resource */
-    int available_resources[NUMBER_OF_RESOURCES];
-    /*the maximum demand of each customer */
-    int max_required[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
-    /* the amount currently allocated to each customer */
-    int allocated_resources[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
-    /* the remaining need of each customer */
-    int remaining_needed[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
-} BANKER;
+int customer_num= 0, count=0;
+
+//from the textbook chapter 8
+/* the available amount of each resource */
+int available_resources[NUMBER_OF_RESOURCES];
+/*the maximum demand of each customer */
+int max_required[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+/* the amount currently allocated to each customer */
+int allocated_resources[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+/* the remaining need of each customer */
+int remaining_needed[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
+
+
+int request_resources(int customer_num, int request[]);
+void release_resources(int customer_num, int release[]);
+
+int main(int argc, char* argv[]){
+    count = argc - 1;
+	int *array = (int*) calloc(count+1, sizeof(int));
+	// printf("%d\n%d\n",argc,count);
+	for (int i=0; i<count; i++){
+        array[i]=atoi(argv[i+1]);
+        // printf("%d\n",array[i]);
+	}
+
+}
 
 
 
